87cf37187d50963c7ba4970f5e95df7023e40664
Nicolas DELCASTILLO <nicolas.delcastillo@syntesy.io>
2020-04-16 12:40:32 +0200

    cleaning code

--- src/NotAttachedException.php
+++ src/NotAttachedException.php
@@ -1,11 +1,10 @@
 <?php
+
 namespace Olea\SSOBroker;
 
 /**
- * Exception thrown when a request is done while no session is attached
- *
+ * Exception thrown when a request is done while no session is attached.
  */
 class NotAttachedException extends Exception
 {
-
 }
